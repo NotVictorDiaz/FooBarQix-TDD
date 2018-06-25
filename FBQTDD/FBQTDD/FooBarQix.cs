b34d9df8e80d18183b5c441c6767db00ab0c2bc0
VictorD07 <victordiazmateo@gmail.com>
2018-06-24 23:08:38 -0400

    Version Inicial

---
+++ FBQTDD/FBQTDD/FooBarQix.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+
+namespace FBQTDD
+{
+    internal class FooBarQix
+    {
+       
+        internal string Compute(string parameter)
+        {
+                int number = Convert.ToInt32(parameter);
+                string test = "";
+
+                if (number % 3 == 0)
+                    test += "Foo";
+                if (number % 5 == 0)
+                    test += "Bar";
+                if (number % 7 == 0)
+                    test += "Qix";
+
+                foreach (var c in parameter)
+                {
+                    if (c == '0')
+                        test += "*";
+                    if (c == '3')
+                        test += "Foo";
+                    if (c == '5')
+                        test += "Bar";
+                    if (c == '7')
+                        test += "Qix";
+                }
+
+                if (test.Contains("Foo") || test.Contains("Bar") || test.Contains("Qix"))
+                    return test;
+
+                parameter = parameter.Replace('0', '*');
+                return parameter;
+        }
+        
+    }
+}
---
+++ FBQTDD/FBQTDD/UnitTest1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FBQTDD
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NumberDivisibleBy3ShouldReturnFoo()
+        {
+            FooBarQix obj = new FooBarQix();
+            string expected = "Foo";
+            string actual = obj.Compute("12");
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+
+        public void NumberDivisibleBy5ShouldReturnBar()
+        {
+            FooBarQix obj = new FooBarQix();
+            string expected = "Bar*";
+            string actual = obj.Compute("20");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+
+        public void NumberDivisibleBy7ShouldReturnQix()
+        {
+            FooBarQix obj = new FooBarQix();
+            string expected = "Qix";
+            string actual = obj.Compute("14");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+
+        public void NumberNonDivisibleByAnyShouldReturnSameNumber()
+        {
+            FooBarQix obj = new FooBarQix();
+            string expected = "1";
+            string actual = obj.Compute("1");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+
+
+        public void ComputeWith60ShouldReturnFooBar()
+        {
+            FooBarQix obj = new FooBarQix();
+            string expected = "FooBar*";
+            string actual = obj.Compute("60");
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ComputeWith15ShouldReturnFooBarBar()
+        {
+            FooBarQix obj = new FooBarQix();
+            string expected = "FooBarBar";
+            string actual = obj.Compute("15");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ComputeWith35ShouldReturnBarQixFooBar()
+        {
+            FooBarQix obj = new FooBarQix();
+            string expected = "BarQixFooBar";
+            string actual = obj.Compute("35");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ComputeWith7ShouldReturnQixQix()
+        {
+            FooBarQix obj = new FooBarQix();
+            string expected = "QixQix";
+            string actual = obj.Compute("7");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ComputeWith105ShouldReturnFooBarQixAsteriskBar()
+        {
+            FooBarQix obj = new FooBarQix();
+            string expected = "FooBarQix*Bar";
+            string actual = obj.Compute("105");
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void ComputeWith101ShouldReturn1Asterisk1()
+        {
+            FooBarQix obj = new FooBarQix();
+            string expected = "1*1";
+            string actual = obj.Compute("101");
+            Assert.AreEqual(expected, actual);
+
+        }
+
+    }
+}
